fceb827e4af6872901aadfaa2e3904c6c81d74bc
Bryan Silva <brandonb2816@gmail.com>
2020-07-09 18:59:50 -0300

    Documentando con PropTypes

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ function App() {
 
   // UseEffect para realizar ciertas operaciones cuando el state cambia
   useEffect( () => {
+    let citasIniciales = JSON.parse( localStorage.getItem('citas') );
+
     if(citasIniciales) {
       localStorage.setItem('citas', JSON.stringify(citas))
     } else {
       localStorage.setItem('citas', JSON.stringify([]));
     }
-  }, [citas, citasIniciales] );
+  }, [citas] );
 
   // Funcion que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
